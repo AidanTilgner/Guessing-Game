7c83555db8a473941f622733ac889fd3a33ede97
Aidan Tilgner <aidantilgner02@gmail.com>
2021-10-19 00:24:13 -0700

    refactoring to STL arrays
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <ctime>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <array>
 
 void play_game();
+void print_array(std::array<int, 251>, int count);
 
 int main() {
   srand(time(NULL)); // random number generator
@@ -33,21 +36,34 @@ int main() {
 }
 
 void play_game() {
+  std::array<int, 251> guesses; // number of guesses should have space for 250 elements
+  int count = 0;
+
   std::cout << "Game is being played." << std::endl;
   int random = rand() % 251; // random number generator
-  std::cout << "Guess a number: " << std::endl;
+  std::cout << "Guess a number between 0 and 250: " << std::endl;
 
   while (true) {
     int guess;
     std::cin >> guess;
+
+    guesses[count++] = guess;
+
     if (guess == random) {
       std::cout << "You win!" << std::endl;
       break;
     } else if (guess < random) {
-      std::cout << "Too low! Guess Again: " << std::endl; 
-    }
-    else {
+      std::cout << "Too low! Guess Again: " << std::endl;
+    } else {
       std::cout << "Too high! Guess Again: " << std::endl;
     }
+  }
+
+  print_array(guesses, count);
+}
+
+void print_array(std::array<int, 251> array, int count) {
+  for (int i = 0; i < count; i++) {
+    std::cout << "Guess #" << i + 1 << ": " << array[i] << std::endl;
   }
 }
